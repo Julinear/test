b03c14a9cb62ac5cdaad6f10bd614d64953be5fa
PrettyPig <33052315+Julinear@users.noreply.github.com>
2024-08-20 11:51:54 +0800

    Update test.ts
    trefsed

--- test.ts
+++ test.ts
@@ -1,12 +1,32 @@
-static mergeStyle() {
-  return `<style>
-  
-    </style>`;
-}
 
-// 处理事件监听收到的数据
-static async handleOutlookEvent(eventMeeting: { created: Meeting[], updated: Meeting[], deleted: string[] }) {
-  if (eventMeeting.created) {
-    await Promise.all(eventMeeting.created.map(async item => await this.createTask(this.assembleTask(item))));
-  }
-}
+ static mergeStyle() {
+        return `<style>
+        table,
+        td {
+          border: 1px solid rgb(218, 214, 214);
+          border-collapse: collapse;
+        }
+      
+        table td {
+          padding: 10px 30px;
+        }
+      
+        p>table>tbody>tr>td:first-child {
+          background-color: rgb(230, 229, 229);
+          font-weight: 600;
+        }
+      </style>`;
+    }
+ 
+    static assembleTask(meeting: Meeting, task?: Task) {
+        let p = document.createElement('p');
+        p.innerHTML = meeting.body;
+        let tables = p.querySelectorAll('table');
+        let table = document.createElement('table');
+        for (let idx = 0; idx < tables.length; idx++) {
+            if (tables[idx].innerHTML.includes('主题') || tables[idx].innerHTML.includes('Subject')) {
+                table = tables[idx];
+                break;
+            }
+        }
+    }
